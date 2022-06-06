a04c90b6bcac05e39fd1bed414a56b5dfcbd138d
PeterSmokery <PeterSmokery@gmail.com>
2022-06-06 17:31:41 +0300

    second task reworked

--- second_ex.java
+++ second_ex.java
@@ -5,30 +5,32 @@ public class second_ex {
     public static void main(String[] args) throws InputMismatchException {
         System.out.println("Введите натуральное число для n-треугольного числа: ");
         Scanner in = new Scanner(System.in);
+        Long n = null;
         try {
-            long n = in.nextLong();
-            if (n == 0) {
-                System.out.println("нуль не натуральное число ");
-                System.exit(1);
-            }
-            if (n < 0) {
-                System.out.println("число отрицательное");
-                System.exit(1);
-
-            }
-            long tringleNumber = triangular_number_counter(n);
-            String res = "Для треугольного числа с основанием " + n + " значение будет равно: " + tringleNumber;
-            System.out.println(res);
+            n = in.nextLong();
         } catch (InputMismatchException e) {
             System.out.println("это не натуральное число");
+            System.exit(1);
         }
+        if (n == 0) {
+            System.out.println("нуль не натуральное число ");
+            System.exit(1);
+        }
+        if (n < 0) {
+            System.out.println("число отрицательное");
+            System.exit(1);
+
+        }
+        long tringleNumber = triangular_number_counter(n);
+        String res = "Для треугольного числа с основанием " + n + " значение будет равно: " + tringleNumber;
+        System.out.println(res);
         in.close();
 
     }
 
     static long triangular_number_counter(long a) {
         long tringle_number = 0;
-        for (int i = 0; i != a; i++) {
+        for (int i = 0; i != a + 1; i++) {
             tringle_number = tringle_number + i;
         }
         return tringle_number;
